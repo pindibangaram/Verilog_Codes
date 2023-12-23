98c71216f9b9794077afa38d65ee3f8c191301e5
pindibangaram <64683545+pindibangaram@users.noreply.github.com>
2023-12-23 22:35:25 +0530

    Add files via upload

--- Up_Down_Counter/TB/up_down_counter_tb.v
+++ Up_Down_Counter/TB/up_down_counter_tb.v
@@ -1,1 +1,74 @@
+module up_down_counter_tb();
+reg clk,rst,load,updown;
+reg [3:0]data;
+wire [3:0]count;
 
+up_down_counter DUT(clk,rst,load,updown,data,count);
+
+//.........Generating clock logic...........
+initial
+begin
+clk = 1'b0;
+forever 
+#5 clk = ~clk;
+end
+
+//.........Reset Generating logic.........
+task reset;
+begin
+@(negedge clk)
+ rst = 1'b1;
+@(negedge clk)
+ rst = 1'b0;
+end
+endtask
+
+//.....Initialising all inputs............
+task initialize;
+begin
+{rst,load,updown,data} = 7'b1010000;
+end
+endtask
+
+//......Load generating logic............
+task load_in(input [3:0]a);
+begin
+@(negedge clk)
+ load = 1'b1;
+ data = a;
+@(negedge clk)
+ load = 1'b0;
+end
+endtask
+
+//.......Updown logic..........
+initial
+begin
+forever
+#100 updown = ~updown;
+end
+
+//........Generating delay..........
+task delay();
+begin
+#100;
+end
+endtask
+
+//......calling all tasks..........
+initial
+begin
+$monitor ("load = %d , data = %d, count = %d",load,data,count);
+initialize;
+reset;
+delay();
+load_in(4'b1010);
+#50;
+load_in(4'b1000);
+#50;
+load_in(4'b0010);
+
+#500 $finish;
+
+end
+endmodule
