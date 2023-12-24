29e842ff31aac908482f9947121c0bd2ba1984e9
pindibangaram <64683545+pindibangaram@users.noreply.github.com>
2023-12-24 11:26:21 +0530

    Update up_down_counter_8bit.v

--- Up_Down_Counter/RTL/up_down_counter_8bit.v
+++ Up_Down_Counter/RTL/up_down_counter_8bit.v
@@ -1,1 +1,14 @@
-
+module up_down_counter_8bit(clk,rst,load,updown,data_in,count);
+  input clk,rst,load,updown;
+  input [7:0]data_in;
+  output reg [7:0]count;
+  always @(posedge clk)
+    if(rst)
+      count <= 8'b0;
+  else if(load)
+    count <= data_in;
+  else if(updown)
+    count <= count +1'b1;
+  else
+    count <= count - 1'b1;
+endmodule
