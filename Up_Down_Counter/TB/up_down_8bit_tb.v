f30b2f5526a0b98280f61ae98178c36575c95ca6
pindibangaram <64683545+pindibangaram@users.noreply.github.com>
2023-12-24 11:47:27 +0530

    Update up_down_8bit_tb.v

--- Up_Down_Counter/TB/up_down_8bit_tb.v
+++ Up_Down_Counter/TB/up_down_8bit_tb.v
@@ -6,5 +6,56 @@ module up_down_8bit_tb();
 
   initial
     begin
-      clk
+      clk = 1'b0;
+      forever 
+        #5 clk = ~clk;
     end
+  
+task initialize;
+  begin
+    {data_in,load,updown,rst} = 11'b00000010010
+  end
+endtask
+  
+  task reset;
+    begin
+      @(negedge clk)
+      rst = 1'b1;
+      @(negedge clk)
+      rst = 1'b0;
+    end
+  endtask
+
+  task load_in(input [7:0]a);
+    begin
+      @(negedge clk)
+      load = 1'b1;
+      data_in = a;
+      @(negedge clk)
+      load = 1'b0;
+     end
+  endtask
+
+  task delay;
+    begin
+      #100;
+    end
+  endtask
+
+  initial
+    begin
+      $monitor ("load = %d,count = %d",load,count);
+      initialize;
+      reset;
+      delay;
+      load_in(8'd200);
+      delay;
+      delay;
+      load_in(8'd100);
+    end
+  initial
+    #1000 $finish;
+endmodule
+  
+  
+  
