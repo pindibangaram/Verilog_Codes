33b6a8cc50a02fd2bc25359ab3d86427ca39d0d2
pindibangaram <64683545+pindibangaram@users.noreply.github.com>
2023-12-24 11:29:59 +0530

    Update up_down_8bit_tb.v

--- Up_Down_Counter/TB/up_down_8bit_tb.v
+++ Up_Down_Counter/TB/up_down_8bit_tb.v
@@ -2,4 +2,9 @@ module up_down_8bit_tb();
   reg clk,rst,load,updown;
   reg [7:0]data_in;
   wire [7:0]count;
-  
+  up_down_counter_8bit DUT(clk,rst,load,updown,data_in,count);
+
+  initial
+    begin
+      clk
+    end
